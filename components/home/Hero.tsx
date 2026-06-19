2f4b80ca7aa255a37cd7db8f6dafe78d5b7b0b8e
mkrishna09 <m.krishna090405@gmail.com>
2026-06-20 02:33:53 +0530

    Fixed colour

--- components/home/Hero.tsx
+++ components/home/Hero.tsx
@@ -88,7 +88,7 @@ export default function Hero() {
     </div>
 
     {/* Bottom Two Images */}
-    <div className="grid grid-cols-2 gap-4">
+    <div className="grid grid-cols-2 gap-2">
 
       <div className="mt-8 overflow-hidden rounded-3xl shadow-xl">
         <Image
